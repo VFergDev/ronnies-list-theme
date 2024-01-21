a4fb705c5957ed6efbdc1b2e759dd27ff337b4d0
Viana Ferguson <vianaferguson@yahoo.com>
2024-01-20 18:26:31 -0700

    change to front-page

--- functions.php
+++ functions.php
@@ -1,16 +1,18 @@
 <?php
 
 // import css and scripts
-function university_files() {
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+function syte_files() {
+    wp_enqueue_script('main-syte-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('syte_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('syte_extra_styles', get_theme_file_uri('/build/index.css'));
 
-    wp_localize_script( 'main-university-js', 'siteData', array(
+    wp_localize_script( 'main-syte-js', 'syteData', array(
       'root_url' => get_site_url(),
       'nonce' => wp_create_nonce('wp_rest')
     ));
 
   }
+
+add_action('wp_enqueue_scripts', 'syte_files');
